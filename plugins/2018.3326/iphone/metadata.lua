97c445a11bead931f1a23b304a15e01f2a58440f
Scott Harrison <scottrules44@gmail.com>
2023-09-26 23:09:38 -0500

    Max 11.11.3 Updates

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -6,9 +6,9 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = {'IASDKCore', "c++", "sqlite3", "z", "xml2", "FyberAdapter"}, 
+		staticLibs = { "c++", "sqlite3", "z", "xml2", "FyberAdapter"},
 
-		frameworks = {},
+		frameworks = {'IASDKCore'},
 		frameworksOptional = {},
 	},
 	coronaManifest = {
