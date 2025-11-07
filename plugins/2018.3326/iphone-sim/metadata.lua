0e39e373db47208a3d2cdbe5234c99e2ed72ccf8
Scott Harrison <scottrules44@gmail.com>
2025-11-07 11:13:10 -0600

    SDK 13.5 support

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -6,9 +6,9 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = { "c++", "sqlite3", "z", "xml2", "FyberAdapter"},
+		staticLibs = { "c++", "sqlite3", "z", "xml2"},
 
-		frameworks = {'IASDKCore'},
+		frameworks = {'IASDKCore', "AppLovinMediationFyberAdapter"},
 		frameworksOptional = {},
 	},
 }
